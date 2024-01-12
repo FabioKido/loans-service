77f59df53df0fff66b257b69b159dfdd785d6a75
FabioKido <fabiohenryquemesquita@gmail.com>
2024-01-12 17:59:11 -0300

    feat(GetLoansAvailable): created usecase

---
+++ src/application/UseCase/GetLoansAvailable.ts
@@ -0,0 +1,29 @@
+import Loan from "../../domain/Loan"
+
+export default class GetLoansAvailable {
+
+    constructor() {}
+
+    execute(input: Input) {
+        const loans = new Loan()
+        const result = loans.setLoansModality(input.income, input.age, input.location)
+        const outputDTO: Output = {
+            customer: input.name,
+            loans: result
+        }
+        return outputDTO
+    }
+}
+
+type Input = {
+    age: number,
+    cpf: string,
+    name: string,
+    income: number,
+    location: string
+}
+
+type Output = {
+    customer: string,
+    loans: any[]
+}
